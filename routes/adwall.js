14ffcff41f5c263420a726d8a82ed909a18a8ed6
jeremy <goldplatedmarketer@gmail.com>
2017-06-29 21:22:01 -0700

    fixed route in adwall router

--- routes/adwall.js
+++ routes/adwall.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
+const path = require('path');
 
 app.use(express.static('public'));
 router.get('/', function (req, res) {
-    res.sendFile('/Users/jeremy/projects/thinkful_capstone_2/public/adwall.html');
+    res.sendFile(path.resolve('./public/adwall.html'));
 
 })
 
