04ef52ecb71599f70c4b24609ea42735ae0ec4bd
jeremy <goldplatedmarketer@gmail.com>
2017-08-08 22:13:05 -0700

    created env variables for facebook auth

--- routes/adwall.js
+++ routes/adwall.js
@@ -1,38 +1,42 @@
 const path = require('path');
-const { BasicStrategy } = require('passport-http');
 const Strategy = require('passport-facebook').Strategy;
 const express = require('express');
 const bodyParser = require('body-parser');
 const jsonParser = require('body-parser').json();
 const passport = require('passport');
 const { User } = require('../models/usermodel.js');
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const router = express.Router();
 const app = express();
-
-// router.use(jsonParser);
-// router.use(passport.initialize());
+app.use(bodyParser);
 
 router.use(express.static('public'));
+
 passport.use(new Strategy({
-    clientID: '472280536469569',
-    clientSecret: '7d851fff7f9ca432d04716834afa1ca7',
-    callbackURL: 'http://localhost:8080/adwall/facebook/auth/cb'
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: 'http://localhost:8080/adwall/facebook/auth/cb',
+    profileFields: ['id', 'displayName', 'photos', 'email']
+
 },
     function (accessToken, refreshToken, profile, cb) {
         return cb(null, profile);
     }
+
 ));
 
-router.get('/', function (req, res) {
-    console.log('request made for adwall');
+router.get('/public', passport.authenticate('facebook'), function (req, res) {
+    console.log(req.isAuthenticated());
     res.sendFile(path.resolve('./public/adwall.html'));
 });
 
+router.get('/facebook/logout', (req, res) => {
+    req.logout();
+    res.end();
+})
 router.get('/facebook/auth', passport.authenticate('facebook'));
 router.get('/facebook/auth/cb',
     passport.authenticate('facebook', {
-        successRedirect: '/register',
+        successRedirect: '/adwall/public',
         failureRedirect: '/login'
     }));
 passport.serializeUser(function (user, done) {
