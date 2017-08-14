f98941fe2c47ffa27396db822f676cbf530dfe94
jeremy <goldplatedmarketer@gmail.com>
2017-08-13 22:14:13 -0700

    Revert "fixed facebook auth by removing middleware"
    This reverts commit 2c2b5432aa82fb848f396b98d6948bb58a3dcd06.

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 const express = require('express');
-const path = require('path');
-const passport = require('passport');
-const Strategy = require('passport-facebook').Strategy;
 const browserSync = require('browser-sync');
 const app = express();
 const mongoose = require('mongoose');
@@ -15,6 +12,7 @@ const adsRouter = require('./routes/ads');
 const userRouter = require('./routes/users');
 const myadsRouter = require('./routes/myads');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const logoutRouter = require('./routes/logout')
 const successRouter = require('./routes/success');
 const { DATABASE_URL, PORT } = require('./config.js');
@@ -30,7 +28,7 @@ app.use(passport.initialize());
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
 app.use('/dashboard', dashRouter);
-// app.use('/adwall', adwallRouter);
+app.use('/adwall', adwallRouter);
 app.use('/posts', postRouter);
 app.use('/directory', dirRouter);
 app.use('/myads', myadsRouter);
@@ -38,12 +36,7 @@ app.use('/users', userRouter);
 app.use('/logout', logoutRouter);
 app.use('/register-success', successRouter);
 app.use('/api/ads', adsRouter);
-// app.use(bodyParser.json());
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
+app.use(bodyParser.json());
 
 function listening() {
     if (!isProduction) {
@@ -92,47 +85,6 @@ function closeServer() {
         });
     });
 }
-
-passport.use(new Strategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: 'http://localhost:8080/facebook/auth/cb'
-},
-    function (accessToken, refreshToken, profile, cb) {
-        return cb(null, profile);
-    }));
-
-
-passport.serializeUser(function (user, cb) {
-    cb(null, user);
-});
-
-passport.deserializeUser(function (obj, cb) {
-    cb(null, obj);
-});
-
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/facebook/logout', (req, res) => {
-    req.logout();
-    res.end();
-})
-app.get('/facebook/auth', passport.authenticate('facebook'));
-app.get('/facebook/auth/cb',
-    passport.authenticate('facebook', {
-        successRedirect: '/adwall',
-        failureRedirect: '/login'
-    }));
-// public router
-//cannot use passport.auth on other links
-//add this router back to adwall.js
-app.get('/adwall', function (req, res) {
-    console.log(req.isAuthenticated());
-    console.log(req.user);
-    res.sendFile(path.resolve('./public/adwall.html'));
-});
 
 if (require.main === module) {
     runServer().catch(err => console.error(err));
